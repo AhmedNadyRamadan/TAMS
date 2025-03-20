c485a9745a10ad463f2a3dd723b7cfc80a29e0f6
AhmedNadyRamadan <an1438@fayoum.edu.eg>
2025-03-20 18:02:36 +0200

    Initial commit - MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TASM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
